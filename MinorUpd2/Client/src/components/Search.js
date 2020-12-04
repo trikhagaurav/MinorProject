2984007b6ae6621133c9b9cb1fdef611c8cbd82e
trikhagaurav <59408047+trikhagaurav@users.noreply.github.com>
2020-12-04 14:54:10 +0530

    First Commit

---
+++ MinorUpd2/Client/src/components/Search.js
@@ -0,0 +1,198 @@
+import React,{Fragment, useEffect, useState, Component} from "react";
+import { NavItem, Nav, FormControl, Navbar, Media, Table, NavDropdown, MenuItem, Button, Card, CardGroup, CardDeck } from "react-bootstrap";
+import {Link} from 'react-router-dom';
+import Moment from 'react-moment';
+
+const Search = ({ setAuth }) => {
+  const logout = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("token");
+    setAuth(false);
+    //toast.success("Logged out successfully");
+  };
+  const [first_name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [reporttype, setReporttype] = useState("");
+  const [report_date, setReportdate] = useState("");
+  const [description, setDesc] = useState("");
+  const [users, setUsers] = useState([]);
+          
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await fetch(`http://localhost:5001/auth/searchemr/?username=${username}`,
+       {
+        method: "GET",
+        headers: { token:localStorage.token },
+      });
+      const parseRes = await response.json();
+      console.log('search emr');
+      setUsers(parseRes);
+      console.log(parseRes);
+      console.log(parseRes.reporttype);
+      setReporttype(parseRes.reporttype);
+      setReportdate(parseRes.report_date);
+      setDesc(parseRes.description);
+      setAuth(true);
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+async function getName() {
+  try {
+    console.log(`Token in doctor ${localStorage.token}`);
+    const response = await fetch("http://localhost:5001/dashboard/", {
+      method: "GET",
+      headers: { token:localStorage.token },
+    });
+
+    const parseRes = await response.json();
+    console.log(parseRes);
+    setName(parseRes.first_name);
+    setAuth(true);
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+  useEffect(() => {
+    setAuth(true);
+    getName();
+},[]);
+  return (
+    <Fragment>
+      <Navbar width="auto" fixed="top" bg="primary" variant="light">
+        <Navbar.Brand>
+          <Link className="text-white" to="/dashboard" style={{ textDecoration: "none" }}>
+            Hi! {first_name}
+          </Link>
+        </Navbar.Brand>
+        <Nav>
+          <Nav.Link>
+            <Link className="text-white" to="/dashboard" style={{ textDecoration: "none" }}>
+              Home
+            </Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link className="text-white" to="/Profile" style={{ textDecoration: "none" }}>
+              Profile
+            </Link>
+          </Nav.Link>
+          <Nav.Link
+            className="border-left pl-2 ml-auto text-auto"
+            onClick={(e) => logout(e)}
+          >
+            Logout
+          </Nav.Link>
+        </Nav>
+      </Navbar>
+      <br />
+      <br />
+      <br /> <br /> <br />
+      <h1>Search for patient medical record</h1>
+      <form className="d-flex">
+        <input
+          type="text"
+          placeholder="Search"
+          className="mr-sm-2"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <Button variant="success" onClick={onSubmitForm}>
+          Search
+        </Button>
+      </form>
+      <br />
+      <br />
+      <div style={{
+      background: '#ebf3f5',
+      marginBottom: '0',
+    }}>
+      <Table striped bordered hover borderless='true'>
+        <thead>
+          <tr>
+            <th>Report Date</th>
+            <th>Report Type</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            users.map(user => (
+              <tr>
+                <td><Moment format="DD/MM/YYYY">{user.report_date}</Moment></td>
+                <td>{user.reporttype}</td>
+                <td>{user.description}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </Table>
+      </div>
+
+
+
+
+      
+      {/*
+        <ul className="list-unstyled">
+          <Media as="li">
+            <img
+              width={120}
+              height={120}
+              className="mr-3"
+              src="https://cdn5.vectorstock.com/i/1000x1000/06/19/medical-flat-icon-medical-report-vector-6480619.jpg"
+              alt="Generic placeholder"
+            />
+            <Media.Body>
+              {users.map( user => (
+            <Media.Body>
+                <h5>Report Date: {user.report_date}</h5>
+                <h5>Report Type: {user.reporttype}</h5>
+                <p>
+                  Description: {user.description}
+                </p>
+            </Media.Body>
+              ))}
+            </Media.Body>
+          </Media>
+          <br />
+          <Media as="li">
+            <img
+              width={120}
+              height={120}
+              className="mr-3"
+              src="https://cdn5.vectorstock.com/i/1000x1000/06/19/medical-flat-icon-medical-report-vector-6480619.jpg"
+              alt="Generic placeholder"
+            />
+            <Media.Body>
+            <h5>Report Date: {report_date}</h5>
+              <h5>Report Type: {reporttype}</h5>
+              <p>
+                Description: {description}
+              </p>
+            </Media.Body>
+          </Media>
+          <br />
+          <Media as="li">
+            <img
+              width={120}
+              height={120}
+              className="mr-3"
+              src="https://cdn5.vectorstock.com/i/1000x1000/06/19/medical-flat-icon-medical-report-vector-6480619.jpg"
+              alt="Generic placeholder"
+            />
+            <Media.Body>
+            <h5>Report Date: {report_date}</h5>
+              <h5>Report Type: {reporttype}</h5>
+              <p>
+                Description: {description}
+              </p>
+            </Media.Body>
+          </Media>
+              </ul> */}
+    </Fragment>
+  );
+};
+
+export default Search;
---
+++ MinorUpd2/Server/middleware/validInfo.js
@@ -0,0 +1,80 @@
+module.exports = function(req, res, next){
+    console.log(req.body);
+    const {  first_name, middle_name, last_name, mobile_number, phone_number, date_of_birth, blood_group, house_number, street, city, pincode, state, gender, username, password,speciality,
+        hospital_name, dob} = req.body;
+  
+    if (req.path === "/patientregister") {
+      //console.log(!patient_id.length);
+      if (![ first_name ,last_name, mobile_number, phone_number, date_of_birth, blood_group, house_number, street, city, pincode, state, gender, username, password].every(Boolean)) {
+        return res.json("Missing Credentials of patient");
+      }
+      if(first_name.length>20){
+        return res.json("First Name should be less 20 characters");
+      }
+      if(middle_name!="")
+      {
+        if(middle_name.length>20){
+          return res.json("Middle Name should be less 10 characters");
+        }
+      }
+      if(last_name.length>20){
+        return res.json("Last name should be less 20 characters");
+      }
+      if(blood_group.length<2||blood_group.length>3||(blood_group.charAt(blood_group.length-1)!='+'&&(blood_group.charAt(blood_group.length-1)!='-'))){
+        return res.json("Invalid blood group");
+      } 
+      if(gender.length!=1||(gender!='M'&&gender!='F'&&gender!='O')){
+        return res.json("Invalid gender");
+      }
+      if(house_number.length>10){
+        return res.json("House number should be less 10 characters");
+      }
+      if(street.length>15){
+        return res.json("Street should be less 15 characters");
+      }
+      if(city.length>15){
+        return res.json("City should be less 15 characters");
+      }
+      if(state.length>15){
+        return res.json("State should be less 15 characters");
+      }
+      if(pincode.length!=6){
+        return res.json("Invalid pincode. Pincode should be of 6 digits.");
+      }
+      if(mobile_number.length<13||mobile_number.length>14){
+        return res.json("Invalid mobile number. Add mobile number with country code. Example- +918865432189");
+      }
+      if(phone_number.length!=11){
+        return res.json("Invalid phone number. Add phone number with state code. Example- 01126455669");
+      }
+      if(username.length>10){
+        return res.json("Username is too long. Username should be less than 10 characters");
+      }
+      if(password.length<6){
+        return res.json("Password must contain atleast 6 characters");
+      }
+      if(password.length>20){
+        return res.json("Password must contain less 20 characters");
+      }
+      var reg= /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,20}$/;
+      if(!reg.test(password)){
+        return res.json("Password must contain a digit and a special character");
+      }
+    } else if (req.path === "/login") {
+      if (![username, password].every(Boolean)) {
+        return res.json("Missing Credentials");
+      } 
+    }
+
+    if(req.path === "/register"){
+        console.log(!username.length);
+        if([first_name, middle_name, last_name, mobile_number, phone_number, speciality, hospital_name, gender, username, password, dob].every[Boolean]){
+            return res.status(401).json("Missing credentials");
+        }
+    } else if (req.path === "/login"){
+        if(![username, password].every(Boolean)) {
+            return res.status(401).json("Missing credentials");
+        }
+    }
+    next();
+};
